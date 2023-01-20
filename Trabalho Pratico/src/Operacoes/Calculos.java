c7c3e634f39a774cd4223d96b6aa4966120a87b9
Gilberto <gilibotas@gmail.com>
2023-01-20 16:30:33 +0200

    Gilberto Commit 1

--- Trabalho Pratico/src/Operacoes/Calculos.java
+++ Trabalho Pratico/src/Operacoes/Calculos.java
@@ -22,4 +22,44 @@ public class Calculos
 		
 		return lucro;
 	}
+	
+	//METODO PARA CALCULAR O NUMERO TOTAL DE DIAS NA DATA DO TESTE
+	public long calcularDias(String data)
+	{
+		short ano=Short.parseShort(data.substring(6,10));
+		byte mes=Byte.parseByte(data.substring(3,5));
+		byte dia=Byte.parseByte(data.substring(0,2));
+		
+		return (long)(ano*365+mes*30+dia);
+	}
+	
+	//METODO PARA SABER SE O ANO EM QUE O TESTE FOI FEITO E BISSEXTO OU NAO
+	public boolean calcularBissexto(short ano)
+	{
+		if((ano%4==0&&ano%100!=0)||(ano%400==0&&ano%100==0))
+			return true;
+		else
+			return false;
+		
+	}
+
+	//METODO PARA CALCULAR A DATA LIMITE DE UM MES
+	public byte calcularDataLimite(byte mes,boolean bissexto)
+	{
+		if(mes==1||mes==3||mes==5||mes==7||mes==8||mes==10||mes==12)
+			return 31;
+		else
+		{
+			if(mes==4||mes==6||mes==9||mes==11)
+				return 30;
+			else
+			{
+				if(mes==2&&bissexto==true)
+					return 29;
+				else
+					return 28;
+			}
+		}
+	}
+	
 }
